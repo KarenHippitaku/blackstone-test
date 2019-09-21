d2f90522e0d4f65d91e210afec0e373433a74234
KarenHippitaku <hippitaku@gmail.com>
2019-09-21 08:03:30 -0500

    Changed to class component

--- karen-slideshow/src/App.js
+++ karen-slideshow/src/App.js
@@ -3,26 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import Slideshow from './components/slideshow';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor () {
+    super ();
+  }
+  render () {
+
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello there, and welcome
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+      Hello there, and welcome
+      </p>
+      <p className="App-link">
+      Keep learning
+      </p>
       </header>
       <Slideshow/>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
