7e79484f165db0c05a5fcfe0d30171cddd14ac38
KarenHippitaku <hippitaku@gmail.com>
2019-09-20 22:00:03 -0500

    Updates

---
+++ precache-manifest.44c8b859736f320b0d5dbb3c7aa0394d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54af6683dcf6fbcea721bdece86f2d5",
+    "url": "/blackstone-test/index.html"
+  },
+  {
+    "revision": "d98af76b4ddd1b677a00",
+    "url": "/blackstone-test/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "68fd624adeb28791a30a",
+    "url": "/blackstone-test/static/css/main.86aac85f.chunk.css"
+  },
+  {
+    "revision": "d98af76b4ddd1b677a00",
+    "url": "/blackstone-test/static/js/2.ce27226c.chunk.js"
+  },
+  {
+    "revision": "68fd624adeb28791a30a",
+    "url": "/blackstone-test/static/js/main.70e8c880.chunk.js"
+  },
+  {
+    "revision": "e19ee32cf11bffdbd905",
+    "url": "/blackstone-test/static/js/runtime-main.0b32ca9b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/blackstone-test/static/media/logo.25bf045c.svg"
+  }
+]);
