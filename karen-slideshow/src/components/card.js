4fd0a6ced230344b4e5aee1e06d0edd79eb754a2
KarenHippitaku <hippitaku@gmail.com>
2019-09-20 22:03:20 -0500

    card with props

--- karen-slideshow/src/components/card.js
+++ karen-slideshow/src/components/card.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './card.css';
+import Button from './button';
 
-function Card() {
+const Card = (props) => {
   return (
-    
+    <dl className="card">
+      <dt className="card-header">
+        <img src={props.picture} alt={props.name}/>
+        <h3>{props.name}</h3>
+      </dt>
+      <dt className="card-body">
+      <img/>
+        <p>{props.debut}</p>
+        <p>{props.episodes}</p>
+        <p>{props.voice}</p>
+      </dt>
+    </dl>
   )
 }
+
+export default Card;
